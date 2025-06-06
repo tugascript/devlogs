bd53be5ba3795d4dc80caac81b348e00dc77059b
Afonso Barracha <barracha.afonso@gmail.com>
2025-06-07 11:49:51 +1200

    fix(auth): remove oauth routes

--- idp/internal/server/routes/auth.go
+++ idp/internal/server/routes/auth.go
@@ -22,13 +22,4 @@ func (r *Routes) AuthRoutes(app *fiber.App) {
 	router.Post(paths.AuthLogin2FA, r.controllers.TwoFAAccessClaimsMiddleware, r.controllers.TwoFactorLoginAccount)
 	router.Post(paths.AuthRefresh, r.controllers.RefreshAccount)
 	router.Post(paths.AuthLogout, r.controllers.AccountAccessClaimsMiddleware, r.controllers.LogoutAccount)
-
-	// Known auth paths (oauth2)
-	router.Post(paths.OAuthKeys, r.controllers.AccountOAuthPublicJWKs)
-	router.Post(paths.AuthOAuthToken, r.controllers.AccountOAuthToken)
-
-	// OAuth2 log ins
-	router.Post(paths.AuthOAuthAppleCallback, r.controllers.AccountAppleCallback)
-	router.Get(paths.AuthOAuthURL, r.controllers.AccountOAuthURL)
-	router.Get(paths.AuthOAuthCallback, r.controllers.AccountOAuthCallback)
 }
